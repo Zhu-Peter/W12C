173dcf84b8d00fb4405c3b4181693541ea96e283
Zhu-Peter <gzhu@ualberta.ca>
2024-04-23 20:28:57 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+axios.request({  
+	url: `https://jsonplaceholder.typicode.com/posts`,  
+}).then(successFunction).catch(failureFunction);
+
+function successFunction(request){
+
+}
+
+function failureFunction(error){
+
+}
