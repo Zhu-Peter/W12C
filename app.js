caf8c98c11e1685f29d6136c63b0eba56cb8ff23
Zhu-Peter <gzhu@ualberta.ca>
2024-04-23 20:39:55 -0600

    added GET posts and formatting

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-axios.request({  
-	url: `https://jsonplaceholder.typicode.com/posts`,  
-}).then(successFunction).catch(failureFunction);
+axios
+  .request({
+    url: `https://jsonplaceholder.typicode.com/posts`,
+  })
+  .then(postPosts)
+  .catch(failureFunction);
 
-function successFunction(request){
+function postPosts(request) {
+    // console.log(request)
+  let posts = request.data
+  for(let i=0; i<posts.length; i++){
+      document.getElementById(`posts`).insertAdjacentHTML(
+        `beforeend`,
+        `
+        <div class="post">
+        <h1>${posts[i].title}</h1>
+        <p>${posts[i].body}</p>
+        </div>
+        `
+      );
 
+  }
 }
 
-function failureFunction(error){
-
+function failureFunction(error) {
+  console.log(error);
 }
